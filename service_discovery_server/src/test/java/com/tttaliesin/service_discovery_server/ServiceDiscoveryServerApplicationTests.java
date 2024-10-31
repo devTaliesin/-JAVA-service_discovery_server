c7a564cd83543e9511f045ece05ac69e9835137a
bict-scpark <scpark@bict.co.kr>
2024-10-31 13:25:02 +0900

    first commit

---
+++ service_discovery_server/src/test/java/com/tttaliesin/service_discovery_server/ServiceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tttaliesin.service_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
